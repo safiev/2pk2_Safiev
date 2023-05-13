87f70ae40617679dfa0dc4cb1800ce62fbecae1a
Кирилл Абельганс <kabelgans.2017@gmail.com>
2023-05-13 12:58:16 +0300

    Добавьте файлы проекта.

---
+++ pz_21_/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_21_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public class MyClass
+        {
+            private int _field1;
+            private string _field2;
+
+            
+            public MyClass()
+            {
+                _field1 = 0;
+                _field2 = "default value";
+            }
+
+           
+            public MyClass(int value)
+            {
+                _field1 = value;
+                _field2 = "default value";
+            }
+
+           
+            public MyClass(int value, string text)
+            {
+                _field1 = value;
+                _field2 = text;
+            }
+
+       
+            public int GetField1()
+            {
+                return _field1;
+            }
+        }
+      
+
+        public class ChemicalElement
+        {
+            private string name;
+            private int countElectrons;
+            private double mass;
+            private int valence;
+
+           
+            public ChemicalElement()
+            {
+                name = "Unknown";
+                countElectrons = 0;
+                mass = 0;
+                valence = 0;
+            }
+
+          
+            public ChemicalElement(string name, int countElectrons, double mass, int valence)
+            {
+                this.name = name;
+                this.countElectrons = countElectrons;
+                this.mass = mass;
+                this.valence = valence;
+            }
+
+            
+            public void PrintInfo()
+            {
+                Console.WriteLine($"Element: {name}, Electrons: {countElectrons}, Mass: {mass}, Valence: {valence}");
+            }
+        }
+
+    }
+}
+    
+
---
+++ pz_22/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public class ChemicalElement
+        {
+            private string name;
+            private int countElectrons;
+            private double mass;
+            private ValenceEnum valence;
+
+            private static int countValence1 = 0;
+            private static int countValence2 = 0;
+
+            public ChemicalElement(string name, int countElectrons, double mass, ValenceEnum valence = ValenceEnum.I)
+            {
+                this.name = !string.IsNullOrEmpty(name) ? name : throw new ArgumentNullException(nameof(name));
+                this.countElectrons = countElectrons > 0 ? countElectrons : throw new ArgumentOutOfRangeException(nameof(countElectrons), "Count of electrons must be greater than zero");
+                this.mass = mass > 0 ? mass : throw new ArgumentOutOfRangeException(nameof(mass), "Mass must be greater than zero");
+                this.valence = valence;
+
+                if (valence == ValenceEnum.I) countValence1++;
+                if (valence == ValenceEnum.II) countValence2++;
+            }
+
+            public string Name { get { return name; } }
+            public int CountElectrons { get { return countElectrons; } }
+            public double Mass { get { return mass; } }
+            public ValenceEnum Valence { get { return valence; } }
+
+            public void PrintInfo()
+            {
+                Console.WriteLine($"Name: {name}, Count of Electrons: {countElectrons}, Mass: {mass}, Valence: {valence}");
+            }
+
+            public static void PrintValenceCounts()
+            {
+                Console.WriteLine($"Count of elements with valence 1: {countValence1}");
+                Console.WriteLine($"Count of elements with valence 2: {countValence2}");
+            }
+        }
+
+        public enum ValenceEnum
+        {
+            I,
+            II,
+            III,
+            IV,
+            V
+        }
+    }
+}
