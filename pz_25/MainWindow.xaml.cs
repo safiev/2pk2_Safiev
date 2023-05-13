9de757dbdd8f4a46c1e01305858dbeb240d086db
Кирилл Абельганс <kabelgans.2017@gmail.com>
2023-05-13 13:23:26 +0300

    0

---
+++ pz_25/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pz_25
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeComponent();
+
+            foreach (UIElement el in Main.Children)
+                if (el is Button)
+                {
+                    ((Button)el).Click += Button_Click;
+                }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+            if (str == "AC")
+            {
+                Text.Text = "";
+            }
+            else if (str == "=")
+            {
+                string value = new DataTable().Compute(Text.Text, null).ToString();
+                Text.Text = value;
+            }
+            else
+                Text.Text += str;
+        }
+    }
+
+}
